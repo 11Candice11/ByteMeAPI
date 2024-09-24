c38809638786ad07211e99a98d8c683d6f02d497
11Candice11 <storieboek@gmail.com>
2024-09-24 12:52:50 +0200

    swagger updated

--- ByteMeAPI/ByteMeAPI/Program.cs
+++ ByteMeAPI/ByteMeAPI/Program.cs
@@ -15,11 +15,17 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+});
 
 app.UseCors(builder =>
     builder.WithOrigins("http://localhost:8080")
